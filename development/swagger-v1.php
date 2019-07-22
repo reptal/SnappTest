0181817a441ddf6b0210f24089552beeed273f9a
Mehran <mehrann206@gmail.com>
2019-07-22 11:49:58 +0430

    search in all products and categories

---
+++ development/swagger-v1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @\Swagger(
+ *     schemes={"http"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="3.0.0",
+ *         title="Swagger Integration with PHP Laravel",
+ *         description="Integrate Swagger in Laravel application",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="mehranab.dev@gmail.com"
+ *         ),
+ *     ),
+ * )
+ */
